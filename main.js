ff8be2aa384abdd9b69217dee5716819b2a0f1cd
Rizvi Ahmed <rizviahmedrumon@gmail.com>
2022-10-31 09:53:00 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,93 @@
+// console.log("Hellow world")
+
+// 4
+
+// "use strict"
+
+// function MyName(){
+
+//     name= "Rizvi Ahmed";
+// }
+
+
+// 5 (... Spread operator)
+
+
+// let smallCountry= ['Monaco', 'Nauru', 'Tuvalu'];
+
+// let bigCountry= [...smallCountry,'Russia', 'Canada', 'China'];
+
+// console.log(bigCountry)
+
+
+// 6 ([.push]Without Spread operator)
+
+// let smallCountry= ['Monaco', 'Nauru', 'Tuvalu'];
+
+// let bigCountry= ['Russia', 'Canada', 'China'];
+
+// bigCountry.push(smallCountry);
+
+// console.log(bigCountry)
+
+
+// 7 {... rest paramete}
+
+// function Calculation(...numbers){
+
+// let sum=0;
+
+// for(let i of numbers){
+
+//     sum=sum+i; 
+// }
+// console.log(sum);
+// }
+
+// Calculation(1,2,3,4,5)
+
+
+// 8 Dynamic function
+
+// var number= function(numberValue){
+
+// return numberValue;
+
+// }
+
+// console.log(number('20'))
+
+
+// 10 variable scope (Local, Global)
+
+// [Local variable scope]
+
+function myFun(){
+
+    var name="rizvi Ahmed";
+
+    console.log(name)
+}
+
+myFun();
+
+// [Global variable scope]
+
+var name="Hey what's up!";
+
+function myFun(){
+
+    console.log(name)
+}
+
+myFun();
+
+// another way
+
+var name="Hey what's up!";
+
+function myFun(){
+
+}
+
+console.log(name)
